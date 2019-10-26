320a7395ccb01b2146356980f53a644540212cdc
Rafael Alexander Reyes Perez <rafaelalexanderr5@gmail.com>
2019-10-26 12:41:10 -0400

    Add the button press to the output screen

---
+++ js/app.js
@@ -0,0 +1,48 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete() {
+
+    }
+    appendNumber(number) {
+        this.currentOperand = number;
+
+    }
+    chooseOperation(operations) {
+
+    }
+    compute() {
+
+    }
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationsButtons = document.querySelectorAll('[data-operations]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+let previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
